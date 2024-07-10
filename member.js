124e3e81428c5788452d5215f832446cc27e66df
Robert Cayouette <159034775+rcayouette3@users.noreply.github.com>
2024-07-10 11:29:02 +0000

    new commit

---
+++ member.js
@@ -0,0 +1,22 @@
+function skillsMembers() {
+    const members = [
+        {
+            name: 'John Doe',
+            skills: ['JavaScript', 'React', 'Node']
+        },
+        {
+            name: 'Jane Doe',
+            skills: ['JavaScript', 'React', 'Node']
+        },
+        {
+            name: 'Jim Doe',
+            skills: ['JavaScript', 'React', 'Node']
+        },
+        {
+            name: 'Jill Doe',
+            skills: ['JavaScript', 'React', 'Node']
+        },
+    ];
+
+    return members;
+}
